c36e8dfd928931cf13d40aecca637dfdcb6696fb
vijay9711 <vijaysm97@gmail.com>
2023-10-10 10:33:47 -0500

    First commit

---
+++ src/login.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class login {
+    private JLabel Email;
+    private JPanel email;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+    private JButton cancelButton;
+    private JLabel Password;
+
+    private void createUIComponents() {
+//        commit test
+        // TODO: place custom component creation code here
+    }
+}
