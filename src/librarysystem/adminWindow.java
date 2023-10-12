2f49ca30f0ef6bba567f586bc543f9c6386973f8
Dip Ranjon Das <dip06ece@gmail.com>
2023-10-12 00:14:01 -0500

    Added Edit member UI and fixed UI break issue of Add member and Edit Member.

--- src/librarysystem/adminWindow.java
+++ src/librarysystem/adminWindow.java
@@ -1,12 +1,12 @@
 package librarysystem;
 
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
@@ -20,7 +20,8 @@ public class adminWindow extends JFrame implements LibWindow {
 	private boolean isInitialized = false;
 	public String person = "default";
 	JLabel mainWindowHeaderPanelText;
-	JPanel mainWindowContentPanel;
+	JPanel mainWindowContentPanel1;
+	JPanel mainWindowContentPanel2;
 
 	private JFrame frame;
 	private JTextField am_memberID;
@@ -31,6 +32,16 @@ public class adminWindow extends JFrame implements LibWindow {
 	private JTextField am_state;
 	private JTextField am_zip;
 	private JTextField am_phoneNumber;
+	
+	private JTextField am_memberID2;
+	private JTextField am_firstName2;
+	private JTextField am_lastName2;
+	private JTextField am_street2;
+	private JTextField am_city2;
+	private JTextField am_state2;
+	private JTextField am_zip2;
+	private JTextField am_phoneNumber2;
+
 	/**
 	 * 
 	 * Launch the application.
@@ -109,6 +120,13 @@ public class adminWindow extends JFrame implements LibWindow {
 		mainWindowButtonePanel.add(btnAddMember);
 
 		JButton btnEditMember = new JButton("Edit Member");
+		btnEditMember.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				mainWindowHeaderPanelText.setText("Modify Member Data");
+				sidePanelModifyMember();
+			}
+		});
 		btnEditMember.setBounds(10, 63, 214, 23);
 		mainWindowButtonePanel.add(btnEditMember);
 
@@ -176,12 +194,129 @@ public class adminWindow extends JFrame implements LibWindow {
 		person = text;
 	}
 	
+        private void sidePanelModifyMember() {
+			if(mainWindowContentPanel1!=null)
+				frame.remove(mainWindowContentPanel1);
+			// Working on Edit member 
+			mainWindowContentPanel2 = new JPanel();
+			mainWindowContentPanel2.setBounds(254, 81, 630, 381);
+			frame.getContentPane().add(mainWindowContentPanel2);
+			mainWindowContentPanel2.setLayout(null);
+				
+			JButton btnModifyMemberFinal = new JButton("Modify This Member");
+			btnModifyMemberFinal.addMouseListener(new MouseAdapter() {							//@dip06ece: Modify Member function here
+				@Override
+				public void mouseClicked(MouseEvent e) {										//@vijay: ###
+				}
+			});
+				btnModifyMemberFinal.setBounds(430, 311, 135, 23);
+				mainWindowContentPanel2.add(btnModifyMemberFinal);
+						
+				am_memberID2 = new JTextField();
+				am_memberID2.setEditable(false);
+				am_memberID2.setBounds(85, 60, 182, 20);
+				mainWindowContentPanel2.add(am_memberID2);
+				am_memberID2.setColumns(10);
+						
+				am_firstName2 = new JTextField();
+				am_firstName2.setBounds(85, 118, 182, 20);
+				mainWindowContentPanel2.add(am_firstName2);
+				am_firstName2.setColumns(10);
+						
+				am_lastName2 = new JTextField();
+				am_lastName2.setBounds(85, 177, 182, 20);
+				mainWindowContentPanel2.add(am_lastName2);
+				am_lastName2.setColumns(10);
+						
+				am_street2 = new JTextField();
+				am_street2.setBounds(85, 240, 182, 20);
+				mainWindowContentPanel2.add(am_street2);
+				am_street2.setColumns(10);
+						
+				am_city2 = new JTextField();
+				am_city2.setBounds(383, 60, 182, 20);
+				mainWindowContentPanel2.add(am_city2);
+				am_city2.setColumns(10);
+						
+				am_state2 = new JTextField();
+				am_state2.setBounds(383, 118, 182, 20);
+				mainWindowContentPanel2.add(am_state2);
+				am_state2.setColumns(10);
+						
+				am_zip2 = new JTextField();
+				am_zip2.setBounds(383, 177, 182, 20);
+				mainWindowContentPanel2.add(am_zip2);
+				am_zip2.setColumns(10);
+						
+				am_phoneNumber2 = new JTextField();
+				am_phoneNumber2.setBounds(383, 240, 182, 20);
+				mainWindowContentPanel2.add(am_phoneNumber2);
+				am_phoneNumber2.setColumns(10);
+						
+				JLabel lblNewLabel_10 = new JLabel("Member ID");
+				lblNewLabel_10.setBounds(85, 45, 182, 14);
+				mainWindowContentPanel2.add(lblNewLabel_10);
+						
+				JLabel lblNewLabel_11 = new JLabel("FirstName");
+				lblNewLabel_11.setBounds(85, 103, 173, 14);
+				mainWindowContentPanel2.add(lblNewLabel_11);
+						
+				JLabel lblNewLabel_12 = new JLabel("Last Name");
+				lblNewLabel_12.setBounds(85, 162, 182, 14);
+				mainWindowContentPanel2.add(lblNewLabel_12);
+						
+				JLabel lblNewLabel_13 = new JLabel("Street");
+				lblNewLabel_13.setBounds(85, 224, 182, 14);
+				mainWindowContentPanel2.add(lblNewLabel_13);
+						
+				JLabel lblNewLabel_14 = new JLabel("City");
+				lblNewLabel_14.setBounds(383, 45, 182, 14);
+				mainWindowContentPanel2.add(lblNewLabel_14);
+						
+				JLabel lblNewLabel_15 = new JLabel("State");
+				lblNewLabel_15.setBounds(383, 103, 182, 14);
+				mainWindowContentPanel2.add(lblNewLabel_15);
+						
+				JLabel lblNewLabel_16 = new JLabel("Zip");
+				lblNewLabel_16.setBounds(383, 162, 182, 14);
+				mainWindowContentPanel2.add(lblNewLabel_16);
+						
+				JLabel lblNewLabel_17 = new JLabel("Phone Number");
+				lblNewLabel_17.setBounds(383, 224, 182, 14);
+				mainWindowContentPanel2.add(lblNewLabel_17);
+				frame.getContentPane().add(mainWindowContentPanel2);
+						
+				JLabel lblNewLabel_18 = new JLabel("Select Member");
+				lblNewLabel_18.setFont(new Font("Tahoma", Font.BOLD, 12));
+				lblNewLabel_18.setBounds(85, 11, 109, 23);
+				mainWindowContentPanel2.add(lblNewLabel_18);
+						
+				JComboBox comboBox1 = new JComboBox();
+				comboBox1.setBounds(191, 12, 267, 22);
+				mainWindowContentPanel2.add(comboBox1);
+						
+				JButton btnModifyMemberInitial = new JButton("Modify");
+				btnModifyMemberInitial.addMouseListener(new MouseAdapter() {   				//@Add event to Modify selected member record
+					@Override
+					public void mouseClicked(MouseEvent e) {
+					}
+				});
+				btnModifyMemberInitial.setBounds(468, 11, 97, 23);
+				mainWindowContentPanel2.add(btnModifyMemberInitial);
+				mainWindowContentPanel2.revalidate();
+				mainWindowContentPanel2.repaint();
+				frame.revalidate();
+				frame.repaint();
+					// Working on Edit member Done
+		}
+
 	private void sidePanelAddMember() {
-		frame.repaint();
-		mainWindowContentPanel = new JPanel();
-		mainWindowContentPanel.setBounds(254, 81, 630, 381);
-		frame.getContentPane().add(mainWindowContentPanel);
-		mainWindowContentPanel.setLayout(null);
+		if (mainWindowContentPanel2!=null)
+			frame.remove(mainWindowContentPanel2);
+		mainWindowContentPanel1 = new JPanel();
+		mainWindowContentPanel1.setBounds(254, 81, 630, 381);
+		frame.getContentPane().add(mainWindowContentPanel1);
+		mainWindowContentPanel1.setLayout(null);
 		
 		JButton btnAddMemberFinal = new JButton("Add This Member");
 		btnAddMemberFinal.addMouseListener(new MouseAdapter() {							//@dip06ece: Add Member function here
@@ -190,79 +325,84 @@ public class adminWindow extends JFrame implements LibWindow {
 			}
 		});
 		btnAddMemberFinal.setBounds(430, 311, 135, 23);
-		mainWindowContentPanel.add(btnAddMemberFinal);
+		mainWindowContentPanel1.add(btnAddMemberFinal);
 		
 		am_memberID = new JTextField();
 		am_memberID.setBounds(85, 60, 182, 20);
-		mainWindowContentPanel.add(am_memberID);
+		mainWindowContentPanel1.add(am_memberID);
 		am_memberID.setColumns(10);
 		
 		am_firstName = new JTextField();
 		am_firstName.setBounds(85, 118, 182, 20);
-		mainWindowContentPanel.add(am_firstName);
+		mainWindowContentPanel1.add(am_firstName);
 		am_firstName.setColumns(10);
 		
 		am_lastName = new JTextField();
 		am_lastName.setBounds(85, 177, 182, 20);
-		mainWindowContentPanel.add(am_lastName);
+		mainWindowContentPanel1.add(am_lastName);
 		am_lastName.setColumns(10);
 		
 		am_street = new JTextField();
 		am_street.setBounds(85, 240, 182, 20);
-		mainWindowContentPanel.add(am_street);
+		mainWindowContentPanel1.add(am_street);
 		am_street.setColumns(10);
 		
 		am_city = new JTextField();
 		am_city.setBounds(383, 60, 182, 20);
-		mainWindowContentPanel.add(am_city);
+		mainWindowContentPanel1.add(am_city);
 		am_city.setColumns(10);
 		
 		am_state = new JTextField();
 		am_state.setBounds(383, 118, 182, 20);
-		mainWindowContentPanel.add(am_state);
+		mainWindowContentPanel1.add(am_state);
 		am_state.setColumns(10);
 		
 		am_zip = new JTextField();
 		am_zip.setBounds(383, 177, 182, 20);
-		mainWindowContentPanel.add(am_zip);
+		mainWindowContentPanel1.add(am_zip);
 		am_zip.setColumns(10);
 		
 		am_phoneNumber = new JTextField();
 		am_phoneNumber.setBounds(383, 240, 182, 20);
-		mainWindowContentPanel.add(am_phoneNumber);
+		mainWindowContentPanel1.add(am_phoneNumber);
 		am_phoneNumber.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Member ID");
 		lblNewLabel_2.setBounds(85, 45, 182, 14);
-		mainWindowContentPanel.add(lblNewLabel_2);
+		mainWindowContentPanel1.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("FirstName");
 		lblNewLabel_3.setBounds(85, 103, 173, 14);
-		mainWindowContentPanel.add(lblNewLabel_3);
+		mainWindowContentPanel1.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("Last Name");
 		lblNewLabel_4.setBounds(85, 162, 182, 14);
-		mainWindowContentPanel.add(lblNewLabel_4);
+		mainWindowContentPanel1.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Street");
 		lblNewLabel_5.setBounds(85, 224, 182, 14);
-		mainWindowContentPanel.add(lblNewLabel_5);
+		mainWindowContentPanel1.add(lblNewLabel_5);
 		
 		JLabel lblNewLabel_6 = new JLabel("City");
 		lblNewLabel_6.setBounds(383, 45, 182, 14);
-		mainWindowContentPanel.add(lblNewLabel_6);
+		mainWindowContentPanel1.add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_7 = new JLabel("State");
 		lblNewLabel_7.setBounds(383, 103, 182, 14);
-		mainWindowContentPanel.add(lblNewLabel_7);
+		mainWindowContentPanel1.add(lblNewLabel_7);
 		
 		JLabel lblNewLabel_8 = new JLabel("Zip");
 		lblNewLabel_8.setBounds(383, 162, 182, 14);
-		mainWindowContentPanel.add(lblNewLabel_8);
+		mainWindowContentPanel1.add(lblNewLabel_8);
 		
 		JLabel lblNewLabel_9 = new JLabel("Phone Number");
 		lblNewLabel_9.setBounds(383, 224, 182, 14);
-		mainWindowContentPanel.add(lblNewLabel_9);
-		frame.getContentPane().add(mainWindowContentPanel);
+		mainWindowContentPanel1.add(lblNewLabel_9);
+		frame.getContentPane().add(mainWindowContentPanel1);
+		
+		mainWindowContentPanel1.revalidate();
+		mainWindowContentPanel1.repaint();
+		frame.revalidate();
+		frame.repaint();
 	}
 }
